90ff1d5f4d6f1a5d823c6b94b38742be30785bb6
Lim_heejin <86215408+heejvely@users.noreply.github.com>
2021-12-22 17:10:57 +0900

    Add files via upload

---
+++ Excercise/py_file/크레인 인형뽑기.py
@@ -0,0 +1,14 @@
+# 크레인 인형뽑기
+def solution(board, moves):
+    bucket = []
+    answer = 0
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move - 1] > 0:
+                bucket.append(board[i][move - 1])
+                board[i][move - 1] = 0
+                if bucket[-1:] == bucket[-2:-1]:
+                    bucket = bucket[:-2]
+                    answer += 2
+                break
+    return answer
