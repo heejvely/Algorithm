447e608416256b673ed20ed0497975ce2eeed434
Lim_heejin <86215408+heejvely@users.noreply.github.com>
2021-12-23 17:42:29 +0900

    Add files via upload

---
+++ Excercise/py_file/더 맵게.py
@@ -0,0 +1,10 @@
+from heapq import *
+def solution(scoville, K):
+    heapify(scoville)
+    count = 0
+    while scoville[0] < K and len(scoville) > 1:
+        num1 = heappop(scoville)
+        num2 = heappop(scoville)
+        heappush(scoville, num1 + num2*2)
+        count += 1
+    return count if scoville[0] >= K else -1
