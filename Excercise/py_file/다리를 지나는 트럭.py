35b9d0b1ca6a15703daa3998c84bcec90766a36c
Lim_heejin <86215408+heejvely@users.noreply.github.com>
2021-12-29 22:51:21 +0900

    Add files via upload

---
+++ Excercise/py_file/다리를 지나는 트럭.py
@@ -0,0 +1,14 @@
+def solution(bridge_length, weight, truck_weights):
+    bridge = [0] * bridge_length
+    time = 0
+
+    while len(bridge) != 0:
+        time += 1
+        bridge.pop(0)
+
+        if truck_weights:
+            if truck_weights[0] + sum(bridge) <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+    return time
